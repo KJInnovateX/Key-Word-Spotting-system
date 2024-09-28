7f2c67ff90143ba645440fb70b8024cefddcc2a4
Karan Jadhav <100209902+KJInnovateX@users.noreply.github.com>
2024-09-29 04:33:18 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,10 @@
 import os
-from app import app
+from app import create_app
+
+# Call the factory function to create the app instance
+app = create_app()
 
 if __name__ == "__main__":
+    # Get the port from the environment variable or use 5000
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
